548dc9bd50ebc39d3a1fad3b4c6fd7e09631060b
NicolasEngels <nicolasengels17@gmail.com>
2023-05-19 15:20:02 +0200

    add loading

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,9 +1,18 @@
 import { Box } from "@chakra-ui/react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
 import Home from "./pages/Home";
+import Loading from "./components/Loading";
 import './App.css';
 
 function App() {
+
+    const { isLoading } = useAuth0();
+
+    if (isLoading) {
+        return <Loading />;
+    }
+
     return (
         <Box className="App">
             <BrowserRouter>
---
+++ front-end/src/components/Loading.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Box, Text } from "@chakra-ui/react";
+
+const Loading: React.FC = () => {
+    return (
+        <Box>
+            <Text>Loading...</Text>
+        </Box>
+    );
+};
+
+export default Loading;
